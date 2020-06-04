cb6e7b2e87da04c1d8b235488c40f90060a4cef5
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2020-06-04 22:04:21 +0200

    Add copyright header

--- Source/Kubernetes/src/main/java/io/dolittle/moose/kubernetes/ingresses/ICanCreateIngressFilters.java
+++ Source/Kubernetes/src/main/java/io/dolittle/moose/kubernetes/ingresses/ICanCreateIngressFilters.java
@@ -1,3 +1,6 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 package io.dolittle.moose.kubernetes.ingresses;
 
 import java.util.function.Predicate;
